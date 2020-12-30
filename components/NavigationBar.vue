16b874f7a5c11a8ab6eef5235e4cb676ffcaf9ef
Emma Stotz <stotz.emma@gmail.com>
2020-12-30 15:41:21 -0500

    style: shadow-md

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-between bg-gradient-to-r from-indigo-500 via-pink-500 to-blue-400 px-8 py-4">
+  <div class="flex justify-between bg-gradient-to-r from-indigo-500 via-pink-500 to-blue-400 px-8 py-4 shadow-md">
     <h1 class="text-white text-lg font-semibold">Emma Stotz</h1>
     <div class="space-x-4 text-white">
       <nuxt-link to="/">
@@ -14,9 +14,3 @@
     </div>
   </div>
 </template>
-
-<script>
-export default {
-
-}
-</script>
