bc7ab225252c8754c7cda8b74a248acf11940782
Emma Stotz <stotz.emma@gmail.com>
2020-12-30 14:12:01 -0500

    feat: `<NavigationBar>`

---
+++ components/NavigationBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex justify-between bg-gradient-to-r from-indigo-500 via-pink-500 to-blue-400 px-8 py-4">
+    <h1 class="text-white text-lg font-semibold">Emma Stotz</h1>
+    <div class="space-x-4 text-white">
+      <nuxt-link to="/">
+        About Me
+      </nuxt-link>
+      <nuxt-link to="/portfolio">
+        Portfolio
+      </nuxt-link>
+			<nuxt-link to="/contact">
+				Contact
+			</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
