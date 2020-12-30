97a3e4a4243d7e2786c1d5f35deac2cc14bde321
Emma Stotz <stotz.emma@gmail.com>
2020-12-30 15:40:41 -0500

    feat: `<DisplayCard>`

---
+++ components/DisplayCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-white rounded-lg shadow max-w-6xl divide-y">
+      <div class="px-6 py-6">
+        <slot name="title" />
+      </div>
+      <div class="px-6 py-6">
+        <slot name="content" />
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
