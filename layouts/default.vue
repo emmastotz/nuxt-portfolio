e2eca0cf051164cf32e8aeb1c28f2fd2073267d8
Emma Stotz <stotz.emma@gmail.com>
2020-12-30 16:27:21 -0500

    feat: sticky

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,25 @@
 <template>
-  <div>
-    <NavigationBar />
-		<div>
-    	<Nuxt />
-		</div>
+  <div class="bg-gradient-to-bl from-white to-gray-400">
+    <div class="sticky top-0 left-0">
+      <NavigationBar />
+    </div>
+    <div class="mx-auto min-h-screen">
+      <Nuxt />
+    </div>
+    <div class="sticky bottom-0 left-0">
+      <FooterBar />
+    </div>
   </div>
 </template>
 
 <script>
 import NavigationBar from '@/components/NavigationBar.vue'
+import FooterBar from '@/components/FooterBar.vue'
 
 export default {
-  components: { 
+  components: {
     NavigationBar,
+    FooterBar,
   }
 }
 </script>
