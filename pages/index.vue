adbd18c56afada17a1ce7a165597c11c1b2e7ebd
Emma Stotz <stotz.emma@gmail.com>
2020-12-30 16:26:58 -0500

    feat: use `<DisplayCard>`

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="container">
-    Hello World
+  <div class="mx-auto my-8">
+    <DisplayCard> 
+      <template #title>
+        <h2 class="font-semibold">About Me</h2>
+      </template>
+      <template #content>
+        <div class="flex">
+          <!-- <div class="flex-1 rounded-full bg-gray-100">
+            <img src="/static/images/emma.jpg" />
+          </div> -->
+          <p class="prose text-gray-500">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </template>
+    </DisplayCard>
   </div>
 </template>
+
+<script>
+import DisplayCard from '@/components/DisplayCard.vue'
+
+export default {
+  components: {
+    DisplayCard,
+  }  
+}
+</script>
