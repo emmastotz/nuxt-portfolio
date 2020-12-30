17b753cd77c6dcf6e9a4e781fd3b2bbe60d4ac70
Emma Stotz <stotz.emma@gmail.com>
2020-12-30 16:41:31 -0500

    style: `green`, `tracking-wider`

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex justify-between bg-gradient-to-r from-indigo-500 via-pink-500 to-blue-400 px-8 py-4 shadow-md">
-    <h1 class="text-white text-lg font-semibold">Emma Stotz</h1>
+  <div class="flex justify-between bg-gradient-to-r from-indigo-500 via-green-300 to-blue-400 px-8 py-4 shadow-md">
+    <h1 class="text-white text-lg font-semibold tracking-wider uppercase">Emma Stotz</h1>
     <div class="space-x-4 text-white">
       <nuxt-link to="/">
         About Me
