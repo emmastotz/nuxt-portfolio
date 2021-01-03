19e28a6d48892d3a3b293578774e1afb788a5996
Emma Stotz <stotz.emma@gmail.com>
2021-01-02 19:53:05 -0500

    feat: bg-gray-200

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
     <!-- Overlay -->
     <main class="-mt-32">
       <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
-        <div class="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
+        <div class="bg-gray-200 rounded-lg shadow px-5 py-6 sm:px-6">
           <div class="rounded-lg min-h-screen">
             <Nuxt />
           </div>
