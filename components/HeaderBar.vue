497c8b8d353014d49947ef52b46a5a9680cae1dd
Emma Stotz <stotz.emma@gmail.com>
2021-01-03 11:00:33 -0500

    fix: data

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -20,62 +20,28 @@
 									</div>
 								</div>
 							</div>
-							<div class="hidden md:block">
-								<div class="ml-4 flex items-center md:ml-6">
-									<button class="bg-gray-800 p-1 text-gray-400 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-										<span class="sr-only">View notifications</span>
-										<!-- Heroicon name: bell -->
-										<svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-											<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-										</svg>
-									</button>
-
-									<!-- Profile dropdown -->
-									<div class="ml-3 relative">
-										<div>
-											<button class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-haspopup="true">
-												<span class="sr-only">Open user menu</span>
-												<img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-											</button>
-										</div>
-										<!--
-											Profile dropdown panel, show/hide based on dropdown state.
-
-											Entering: "transition ease-out duration-100"
-												From: "transform opacity-0 scale-95"
-												To: "transform opacity-100 scale-100"
-											Leaving: "transition ease-in duration-75"
-												From: "transform opacity-100 scale-100"
-												To: "transform opacity-0 scale-95"
-										-->
-										<div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-											<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
-
-											<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
-
-											<a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
-										</div>
-									</div>
-								</div>
-							</div>
 							<div class="-mr-2 flex md:hidden">
 								<!-- Mobile menu button -->
-								<button class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+								<button class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" @click="toggle()">
 									<span class="sr-only">Open main menu</span>
 									<!--
 										Heroicon name: menu
 
 										Menu open: "hidden", Menu closed: "block"
 									-->
-									<MenuSvg class="block w-6 h-auto fill-current text-white" />
+									<MenuSvg 
+										class="w-6 h-auto fill-current text-white" 
+										:class="isOpen ? 'hidden' : 'block'" 
+									/>
 									<!--
 										Heroicon name: x
 
 										Menu open: "block", Menu closed: "hidden"
 									-->
-									<svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-										<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-									</svg>
+									<XSvg 
+										class="w-6 h-auto fill-current text-white" 
+										:class="isOpen ? 'block' : 'hidden'" 
+									/>
 								</button>
 							</div>
 						</div>
@@ -87,7 +53,7 @@
 
 					Open: "block", closed: "hidden"
 				-->
-				<div class="hidden border-b border-gray-700 md:hidden">
+				<div v-if="isOpen" class="hidden border-b border-gray-700 md:hidden">
 					<div class="px-2 py-3 space-y-1 sm:px-3">
 						<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
 						<a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
@@ -150,9 +116,11 @@ export default {
 		MenuSvg,
 		XSvg,
 	},
-	data: {
-		isOpen: false,
-	},
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
 	computed: {
 		headerText() {
 			if(this.$route.path === '/'){
@@ -161,6 +129,11 @@ export default {
 			const path = this.$route.path.split('/')
 			return path[1]
 		}
+	},
+	methods: {
+		toggle() {
+      this.isOpen = !this.isOpen
+    },
 	}
 }
 </script>
