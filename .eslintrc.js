537e8d3944a5ec85a3f9a244aaa263424e458823
Emma Stotz <stotz.emma@gmail.com>
2021-01-03 10:55:59 -0500

    Revert "feat: eslint-rules"
    This reverts commit e423b4d30aefecf53daa09ce20518086c4ef9868.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,72 +1,20 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
-    node: true,
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
+    es2021: true,
+    node: true
   },
   extends: [
-    'plugin:vue/recommended',
-    '@vue/prettier/recommended',
-    'plugin:nuxt/recommended',
+    'plugin:vue/essential',
+    'standard'
   ],
-  plugins: ['vue', 'prettier', 'import', 'sort-imports-es6-autofix'],
-  settings: {
-    'import/resolver': 'nuxt',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
   },
-  // add your custom rules here
+  plugins: [
+    'vue'
+  ],
   rules: {
-    // sloppy code
-    'no-console': 'warn',
-    'no-lonely-if': 'warn',
-
-    // unused code
-    'no-empty': 'warn',
-    'no-unused-vars': [
-      'warn',
-      {
-        ignoreRestSiblings: true,
-      },
-    ],
-    'require-await': 'warn',
-
-    'vue/no-empty-component-block': 'warn',
-    'vue/no-unused-components': 'warn',
-    'vue/no-useless-mustaches': 'warn',
-    'vue/no-useless-v-bind': 'warn',
-
-    // style
-    'curly': 'warn',
-    'lines-between-class-members': 'warn',
-    'no-var': 'warn',
-    'object-shorthand': 'warn',
-    'prefer-const': 'warn',
-
-    'sort-imports-es6-autofix/sort-imports-es6': [
-      'warn',
-      {
-        ignoreCase: false,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-      },
-    ],
-
-    'import/extensions': [
-      'warn',
-      'always',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'import/newline-after-import': 'warn',
-
-    'vue/component-definition-name-casing': ['warn', 'PascalCase'],
-    'vue/component-name-in-template-casing': ['warn', 'PascalCase'],
-    'vue/padding-line-between-blocks': ['warn'],
-  },
+  }
 }
