ae1ee21d8b07a07b063c95f418141b613d72208f
Emma Stotz <stotz.emma@gmail.com>
2021-01-04 10:10:36 -0500

    feat: remove redundant styles

--- components/DisplayCard.vue
+++ components/DisplayCard.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="bg-white rounded-lg shadow divide-y mx-auto">
     <div class="px-6 py-6">
-      <slot name="title" />
+      <h2 class="font-bold tracking-wider text-yellow-900 text-center">
+        <slot name="title" />
+      </h2>
     </div>
-    <div class="px-6 py-6">
+    <div class="px-6 py-6 text-gray-500 text-center">
       <slot name="content" />
     </div>
   </div>
