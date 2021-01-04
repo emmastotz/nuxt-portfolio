4ee10053ff5f28fd15dcb071f372e36d607a7e55
Emma Stotz <stotz.emma@gmail.com>
2021-01-03 23:48:34 -0500

    feat: use `<ContactForm>`

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>Contact</div>
+  <div>
+    <ContactForm />
+  </div>
 </template>
 
 <script>
+import ContactForm from '@/components/ContactForm.vue'
 export default {
-
+  components: {
+    ContactForm,
+  }
 }
 </script>
