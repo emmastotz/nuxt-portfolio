478215f002b5e0b5114d59e8c2823ec289c58ddd
Emma Stotz <stotz.emma@gmail.com>
2021-01-04 10:10:52 -0500

    feat: remove redundant styles

--- pages/index.vue
+++ pages/index.vue
@@ -2,70 +2,68 @@
   <div class="mx-8 my-8 space-y-8">
     <div class="flex flex-col justify-between items-center space-y-8 lg:flex-row lg:space-y-0 lg:space-x-8 xl:justify-around">
       <div class="flex-shrink-0 rounded-full shadow-lg p-5 bg-white">
-        <img src="/images/emma.jpg" class="object-cover rounded-full h-32 w-auto lg:h-40"/>
+        <img src="/images/emma.jpg" class="object-cover rounded-full h-32 w-auto lg:h-40 border-2 border-gray-200"/>
       </div>
       <DisplayCard> 
         <template #title>
-          <h2 class="text-lg font-bold tracking-wider text-yellow-900 text-center">Hello, I'm Emma. Nice to meet you.</h2>
+          <h2 class="text-lg">Hello, I'm Emma. Nice to meet you.</h2>
         </template>
         <template #content>
-          <div class="flex">
-            <p class="prose text-gray-500 text-center">
-              Since beginning my journey as a designer nearly 5 years ago I've done remote work for startups, collaborated with Marketing and Brand teams to create digital and print designs for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my abilities one design problem at a time.
-            </p>
-          </div>
+          <p>
+            Since beginning my journey as a designer nearly 5 years ago I've done remote work for startups, collaborated with Marketing and Brand teams to create digital and print designs for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my abilities one design problem at a time.
+          </p>
         </template>
       </DisplayCard>
     </div>
     <div class="flex flex-col space-y-8 md:flex-row md:space-y-0 md:space-x-8">
       <DisplayCard> 
         <template #title>
-          <h2 class="font-bold tracking-wider text-yellow-900 text-center">Designer</h2>
+          Designer
         </template>
         <template #content>
           <div class="grid gap-6">
-            <p class="text-gray-500 text-center">
+            <p>
               I value simple content structure, clean design patterns, and thoughtful interactions.
             </p>
-            <div class="text-center">
+            <div>
               <h3 class="text-yellow-900 font-semibold">Things I enjoy designing:</h3>
-              <p class="text-gray-500">
+              <p>
                 UX, UI, Mobile, Apps, Logos
               </p>
             </div>
-            <ul class="text-center">
+            <ul>
               <span class="text-yellow-900 font-semibold">Design Tools:</span>
-              <li class="text-gray-500">Figma</li>
-              <li class="text-gray-500">Adobe Creative Cloud</li>
-              <li class="text-gray-500">Balsamiq</li>
-              <li class="text-gray-500">Pen & Paper</li>
-              <li class="text-gray-500">Sketch</li>
+              <li>Figma</li>
+              <li>Adobe Creative Cloud</li>
+              <li>Balsamiq</li>
+              <li>Pen & Paper</li>
+              <li>Sketch</li>
             </ul>
           </div>
         </template>
       </DisplayCard>
       <DisplayCard> 
         <template #title>
-          <h2 class="font-bold tracking-wider text-yellow-900 text-center">Front-end Developer</h2>
+          Frontend Developer
         </template>
         <template #content>
           <div class="grid gap-6">
-            <p class="text-gray-500 text-center">
+            <p>
               I like to code things from scratch, and enjoy bringing ideas to life in the browser.
             </p>
-            <div class="text-center">
+            <div>
               <h3 class="text-yellow-900 font-semibold">Languages I speak:</h3>
-              <p class="text-gray-500">
-                HTML, CSS, Javascript, Java, Vue, Tailwind, React
+              <p>
+                HTML, CSS, Javascript, Vue, Tailwind, React, Java
               </p>
             </div>
-            <ul class="text-center">
+            <ul>
               <span class="text-yellow-900 font-semibold">Dev Tools:</span>
-              <li class="text-gray-500">Github</li>
-              <li class="text-gray-500">Visual Studio Code</li>
-              <li class="text-gray-500">IntelliJ</li>
-              <li class="text-gray-500">Stack Overflow</li>
-              <li class="text-gray-500">Gitbash</li>
+              <li>Github</li>
+              <li>Visual Studio Code</li>
+              <li>IntelliJ</li>
+              <li>Stack Overflow</li>
+              <li>Gitbash</li>
             </ul>
           </div>
         </template>
