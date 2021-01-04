9007d8ced95e3b2f80a4e60eb56e14ec776f3ebe
Emma Stotz <stotz.emma@gmail.com>
2021-01-04 13:05:58 -0500

    feat: use renamed component

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,14 +1,35 @@
 <template>
-  <div>
-    <ContactForm />
+  <div class="mt-10 sm:mt-0">
+    <div class="md:grid md:grid-cols-3 md:gap-6">
+      <div class="md:col-span-1">
+        <div class="px-4 sm:px-0">
+          <h3 class="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
+          <p class="mt-1 text-sm text-gray-600">
+            This information will only be seen by yours truly.
+          </p>
+        </div>
+      </div>
+      <div class="mt-5 md:mt-0 md:col-span-2">
+        <form action="#" method="POST">
+          <div class="overflow-hidden sm:rounded-md">
+          <ContactInput />
+            <div class="px-4 py-3 bg-gray-50 text-left sm:px-6">
+              <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-yellow-900 hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-800">
+                Send
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import ContactForm from '@/components/ContactForm.vue'
+import ContactInput from '@/components/ContactInput.vue'
 export default {
   components: {
-    ContactForm,
+    ContactInput,
   }
 }
 </script>
