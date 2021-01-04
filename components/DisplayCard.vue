00bf5004cc7fb60c4c9ecbf41beaf6098ad9df15
Emma Stotz <stotz.emma@gmail.com>
2021-01-03 23:47:23 -0500

    feat: remove fixed width

--- components/DisplayCard.vue
+++ components/DisplayCard.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="bg-white rounded-lg shadow max-w-2xl divide-y mx-auto">
-      <div class="px-6 py-6">
-        <slot name="title" />
-      </div>
-      <div class="px-6 py-6">
-        <slot name="content" />
-      </div>
+  <div class="bg-white rounded-lg shadow divide-y mx-auto">
+    <div class="px-6 py-6">
+      <slot name="title" />
+    </div>
+    <div class="px-6 py-6">
+      <slot name="content" />
+    </div>
   </div>
 </template>
 
