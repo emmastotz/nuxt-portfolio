62dee2ac23051111617ef1ab5645a6e0684a1a96
Emma Stotz <stotz.emma@gmail.com>
2021-01-04 20:56:13 -0500

    feat: use `<PortfolioCard>`

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,9 +1,47 @@
 <template>
-  <div>Portfolio</div>
+  <div class="grid grid-cols-3 space-x-6">
+    <PortfolioCard v-for="project in projects" :key="project.id" v-bind="{ project }" />
+  </div>
 </template>
 
 <script>
-export default {
+import PortfolioCard from '@/components/PortfolioCard.vue'
+import WhitehawkSvg from '@/components/icons/WhitehawkSvg.vue'
 
+export default {
+  components: {
+    PortfolioCard,
+    WhitehawkSvg,
+  },
+  data() {
+    return {
+      projects: [
+        {
+          id: 1,
+          name: 'WhiteHawk CEC',
+          description: ['Front-end development', 'UI design'],
+          link: 'https://whitehawk.com/',
+          image: '/images/computer.jpg',
+          date: 'Jan 2020 - Current',
+        },
+        {
+          id: 2,
+          name: 'Southwest Spirits',
+          description: ['Marketing material development', 'Product design'],
+          link: '/portfolio/southwest-spirits',
+          image: '/images/mule.jpg',
+          date: 'May 2019 - Feb 2020 ',
+        },
+        {
+          id: 3,
+          name: 'Virginia Distillery Co.',
+          description: ['Brand development', 'Marketing material development', 'Product design'],
+          link: '/portfolio/virginia-distillery-co',
+          image: '/images/distillery.jpg',
+          date: 'Sep 2016 - Oct 2019',
+        },
+      ]
+    }
+  },
 }
 </script>
